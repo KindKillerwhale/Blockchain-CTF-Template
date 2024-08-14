126192b35e2159f01d10c24b7f455c1db7ffab0e
Kind_Killerwhale <hdh317817@gmail.com>
2024-08-14 22:59:41 +0900

    bug fix and insert automatic request

--- ex.py
+++ ex.py
@@ -1,17 +1,30 @@
 from web3 import Web3
+import requests
+
+
+#response = requests.get('http://rpc_url:port/info')
+#request_data = response.json()
+
+#contract_add = request_data['contractAddress']
+#private_key = request_data['privateKey']
+#rpc_url = request_data['rpcUrl']
+#account_address = request_data['walletAddress']
+
+#print(contract_add, private_key, rpc_url, account_address)
+
 
 account_address = 'ACCOUNT_ADDRESS'
 private_key = 'PRIVATE_KEY'
 rpc_url = 'RPC_URL'
 web3 = Web3(Web3.HTTPProvider(rpc_url))
-print("Are we connected?", web3.isConnected())
+print("Are we connected?", web3.is_connected())
 
 contract_abi = [YOUR CONTRACT ABI]
 contract_add = "CONTRACT_ADDR"
 
 example = web3.eth.contract(address=contract_add, abi=contract_abi)
 
-gas_price = web3.toWei('20', 'gwei')
+gas_price = web3.to_wei('20', 'gwei')
 
 
 # Specify 'gasPrice' because it should follow the 'legacy' format.
